18a641f5096a8d91ee1075f5e39d040346cac4e5
Ankur Gupta <ankurnecessary@gmail.com>
2024-02-07 06:21:50 +1030

    Added player component to set the player name

---
+++ src/components/Player.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+
+export default function Player({ initialName, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [name, setName] = useState(initialName);
+
+  function toggleEdit() {
+    setIsEditing((editing) => !editing);
+  }
+
+  function changeName(event) {
+    setName(event.target.value);
+  }
+
+  function onEnter(event) {
+    if (event.code == "Enter") {
+      setIsEditing((editing) => !editing);
+      setName(event.target.value);
+    }
+  }
+
+  return (
+    <li>
+      <span className="player">
+        {!isEditing && <span className="player-name">{name}</span>}
+        {isEditing && (
+          <input
+            type="text"
+            value={name}
+            required
+            onChange={changeName}
+            onKeyDown={onEnter}
+          />
+        )}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={toggleEdit}>{isEditing ? "Save" : "Edit"}</button>
+    </li>
+  );
+}
