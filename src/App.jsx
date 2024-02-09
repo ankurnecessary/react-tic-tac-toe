ee2db78724abaa258284215cac385e118c0285a8
Ankur Gupta <ankurnecessary@gmail.com>
2024-02-10 07:37:19 +1030

    Game draw and game over screens added.

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import GameBoard from "./components/GameBoard";
 import Player from "./components/Player";
 import Log from "./components/Log";
 import { WINNING_COMBINATIONS } from "./components/winning-combinations";
+import GameOver from "./components/GameOver";
 
 const initialGame = [
   [null, null, null],
@@ -68,6 +69,8 @@ function App() {
     });
   }
 
+  let hasDraw = gameTurns.length === 9 && !winner;
+
   return (
     <main>
       <div id="game-container">
@@ -83,7 +86,7 @@ function App() {
             isActive={activePlayer === "O"}
           />
         </ol>
-        {winner && <p>You won, {winner}!</p>}
+        {(winner || hasDraw) && <GameOver winner={winner} />}
         <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
       </div>
       <Log turns={gameTurns} />
